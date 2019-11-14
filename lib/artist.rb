7f15c6003c492d9415aeccec14b0c9857bbbde47
Stacy Johns <stacykjohns@icloud.com>
2019-11-14 19:09:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,21 +11,6 @@ class Artist
     @@all
   end
 
-  def new_song(name, genre)
-    new_song = Song.new(name, genre)
-    @@all << new_song
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-
-  def genres
-    Song.all.select do |song|
-      song.genre
-  end
-end
+  
 
 end
