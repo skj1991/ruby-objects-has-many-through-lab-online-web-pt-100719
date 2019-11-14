73d8917283829152b8cf2759422cc25384df2701
Stacy Johns <stacykjohns@icloud.com>
2019-11-14 19:10:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ class Song
 
     @@all = []
 
-    def initialize(name)
+    def initialize(name, artist, genre)
       @name = name
+      @artist = artist
+      @genre = genre
       @@all << self
     end
 
