f4eedb893ec3bfcaab1edc372343e10592303277
Stacy Johns <stacykjohns@icloud.com>
2019-11-14 19:04:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,14 @@
 class Song
-  attr_accessor :name, :artist, :genre
-  @@all = []
+  attr_accessor :artist, :name
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-  end
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
 end
